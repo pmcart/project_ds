2711b18009bbf25bba86ecb77ee14207b08d50f4
Patrick McArt <patrickmcart@gmail.com>
2017-08-16 22:37:15 +0100

    Latest updates

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,29 +5,7 @@ import {Router} from '@angular/router';
   selector: 'header-component',
   styles: ['.btn-secondary.active { color:white; background-color: #f0ad4e;} .contact-list { list-style-type:none; }  .contact-text{color:#f0ad4e;} .contact-strong{color:white;}'],
   template: `
-  <nav class="navbar fixed-top navbar-toggleable-md navbar-light bg-faded">
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarExample" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="container">
-            <a class="navbar-brand" href="#"></a>
-            <div class="collapse navbar-collapse" id="navbarExample">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" routerLink="/home" routerLinkActive="active">Services</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" routerLink="/contact" routerLinkActive="active">Contact</a>
-                    </li>
-                    <li class="nav-item">
  
-                    </li>
-                </ul>
-            </div>
-       
-        </div>
-    </nav>
-
 
     <header class="intro-header">
         <div class="container">
